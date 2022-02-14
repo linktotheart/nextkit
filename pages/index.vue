ad6e77d4d3078a219c047c117b6ded5306cb0aac
pulkit-lyb <pulkit.lybtechnology@gmail.com>
2022-02-14 23:10:08 +0530

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "NuxtJs UI kit | Free UI kit built with Vuetify",
+      meta: [
+        {
+          hid: "description",
+          name: "Next Ui Kit",
+          content: "Next Ui Kit is the best way",
+        },
+      ],
+    };
+  },
+
+  data() {
+    return {};
+  },
+  components: {
+    Banner: () => import("@/components/shared/lp-banner/Banner"),
+    ComingSoon: () => import("@/components/shared/coming-soon/ComingSoon"),
+    AllCustomComponents: () =>
+      import("@/components/custom/AllCustomComponents"),
+  },
+};
+</script>
